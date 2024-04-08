f03936c7709c464fb0f8a6e099bd8b6b0a9709ab
unknown <patilsaurabh12345@gmail.com>
2024-04-08 21:00:02 +0530

    Investment calculator first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import logo from '../assets/investment-calculator-logo.png';
+
+
+export default function Header(){
+return <header id='header'>
+    <img src={logo} alt="InvestmentCalculatorImage" />
+    <h1>Saurabh's Investment Calculator</h1>
+</header>
+
+}
---
+++ src/components/UserInput.jsx
@@ -0,0 +1,41 @@
+
+export default function UserInput({onChange,userInput}){
+
+    return <section id='user-input'>
+        <div className="input-group">
+            <p>
+                <label >Initial Investment</label>
+                <input type="number" 
+                required 
+                value={userInput.initialInvestment} 
+                //the value retireved in event.target.value will always be the string even though the input type="number" to overcome this add the + sign to the uniqueIdentifier in setUserInput function in the App.jsx file
+                onChange={(event)=>
+                    onChange('initialInvestment',event.target.value)}/>
+            </p>
+            <p>
+                <label >Annual Investment</label>
+                <input type="number" required
+                 value={userInput.annualInvestment} 
+                 onChange={(event)=>
+                    onChange('annualInvestment',event.target.value)}
+                />
+            </p>
+            <p>
+                <label >Expected Return</label>
+                <input type="number" required
+                 value={userInput.expectedReturn} 
+                 onChange={(event)=>
+                    onChange('expectedReturn',event.target.value)}
+                />
+            </p>
+            <p>
+                <label >Duration</label>
+                <input type="number" required
+                 value={userInput.duration} 
+                 onChange={(event)=>
+                    onChange('duration',event.target.value)}                
+                />
+            </p>
+        </div>
+    </section>
+}
